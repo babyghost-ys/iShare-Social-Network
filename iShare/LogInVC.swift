a180e05ccd3e86a121649a1aa1c582674b609a35
Peter Leung <babyghost@gmail.com>
2016-10-02 21:02:46 +0100

    Facebook Login Added.

--- iShare/LogInVC.swift
+++ iShare/LogInVC.swift
@@ -7,9 +7,37 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class LogInVC: UIViewController {
 
+    @IBAction func fbButoonLogin(_ sender: AnyObject) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else if result?.isCancelled == true {
+                print("user cancelled")
+            } else {
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+                print("Auth ok")
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                print("Auth ok - Firebase")
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
